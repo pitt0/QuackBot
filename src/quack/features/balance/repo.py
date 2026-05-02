b8804edc6922ca47a6a1776968e6c6c1143cd41d
pitt0 <pitto.developer@gmail.com>
2026-05-02 12:59:30 +0200

    Fixed balance fetch query

--- src/quack/features/balance/repo.py
+++ src/quack/features/balance/repo.py
@@ -4,25 +4,36 @@ from quack.storage.db import connect
 def fetch_history(group_id: int) -> list[tuple[str, str | None, str, str, int]]:
     with connect() as connection:
         query = """
+        WITH group_members AS (
+            SELECT
+                user_id,
+                COALESCE(user_alias, user_tag) AS user_handle,
+                user_tag
+            FROM
+                users AS usr
+                INNER JOIN group_registrations AS gr
+                ON usr.user_id = gr.user_id
+            WHERE
+                gr.group_id = ?
+        )
+
         SELECT
-            COALESCE(pu.user_alias, pu.user_tag),
+            gme.user_handle,
             pr.purchase_label,
             pr.created_at,
-            u.user_tag,
-            e.amount
+            gme.user_tag,
+            exp.amount
 
         FROM
             purchase_record AS pr
 
-            INNER JOIN expenses AS e
-            USING (purchase_id)
+            INNER JOIN group_members AS gmp
+            ON gmp.user_id = pr.creator_id
 
-            INNER JOIN users AS u
-            ON e.user_id = u.user_id
+            INNER JOIN expenses AS exp
+            ON exp.purchase_id = pr.purchase_id
 
-            INNER JOIN users AS pu
-            ON pr.creator_id = pu.user_id
-        WHERE
-            group_id = ?;
+            INNER JOIN group_members AS gme
+            ON exp.user_id = gme.user_id;
         """
         return connection.execute(query, (group_id,)).fetchall()
